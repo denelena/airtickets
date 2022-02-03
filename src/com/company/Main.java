8d0c1a909ba1d5e64521fa6fb1ee304434d0eaef
Елена Денисова <E-denisova@yandex.ru>
2022-02-03 14:38:09 +0300

    INITIAL COMMIT

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int paidAmount = 100013; // рублей потрачено на билет
+        int amountForMile = 20;// рублей для одной бонусной милли
+
+        int bonusMiles = paidAmount/amountForMile;//Рассчитываем количество бонусных милль, используя
+        // значения заведённых переменных.
+        System.out.println(bonusMiles);// выводим на экран
+    }
+}
